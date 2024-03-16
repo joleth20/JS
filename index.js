51d222660b1c8497c366b8cd96adbb57d997c0fd
Joleth Ayuson <ayuson.joleth.bsinfotech@gmail.com>
2024-03-16 22:28:54 +0800

    added burger nav

--- index.js
+++ index.js
@@ -20,5 +20,28 @@
 // const sampleArray = ['a', 'b', 'c','d']
 
 
-// console.log(sampleArray[2])
+console.log()
 
+
+window.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger')
+    const menu = document.querySelector('.menus')
+
+    hamburger.addEventListener('click', () => {
+        if(hamburger.className.includes('bars')){
+           hamburger.classList.remove('bars')
+           hamburger.classList.add('close')
+         
+           menu.classList.remove('hide')
+           menu.classList.add('show')
+
+        }else{
+            hamburger.classList.add('bars')
+            hamburger.classList.remove('close')
+
+           menu.classList.add('hide')
+           menu.classList.remove('show')
+        }
+    })
+
+})
